b00c4c0921db325fb382408152d42185e7d8d916
Shakir Choudhary <shakirchoudhary2117@gmail.com>
2023-04-28 01:49:19 +0530

    Tried edit but failed

--- src/app/userreg/userreg.component.ts
+++ src/app/userreg/userreg.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { CandidateService } from '../candidate.service';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-userreg',
@@ -9,7 +11,9 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./userreg.component.css'],
 })
 export class UserregComponent implements OnInit {
-  constructor(private candidates: CandidateService, private http: HttpClient) {}
+  EditForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private candidate: CandidateService, private http: HttpClient) { }
   candidatedata: any = [];
   // ngOnInit() {
   //   this.candidates.candidatesget().subscribe(
@@ -27,8 +31,61 @@ export class UserregComponent implements OnInit {
   // constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.EditForm = this.formBuilder.group({
+      Image: ['', Validators.required],
+
+      FName: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-zA-Z ]*'),
+          Validators.maxLength(20),
+        ],
+      ],
+      LName: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-zA-Z ]*'),
+          Validators.maxLength(20),
+        ],
+      ],
+      email: [
+        '',
+        Validators.required,
+        Validators.pattern(
+          "[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*"
+        ),
+      ],
+      age: ['', [Validators.required, Validators.min(18), Validators.max(100)]],
+      interests: [''],
+      addressType: ['', Validators.required],
+      companyAddress1: [''],
+      companyAddress2: [''],
+      homeAddress1: [''],
+      homeAddress2: [''],
+    });
     this.http.get('http://localhost:3000/candidate/').subscribe((data) => {
       this.candidatedata = data;
     });
+  }
+  onClickEdit() {
+    this.http.get('http://localhost:3000/candidate/').subscribe((data) => {
+      this.candidatedata = data;
+      console.log(data)
+    });
+  }
+  onClickSubmit() {
+
+    this.candidate.candidate(this.EditForm.value).subscribe(
+      (result: any) => {
+        console.log('Form data sent to server');
+        console.log(result);
+      },
+      (error: any) => {
+        console.log('Error sending form data to server:', error);
+      }
+    );
+
   }
 }
